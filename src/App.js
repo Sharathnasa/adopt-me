68fb01ff4bb24f1a970ffe0adc02083e81da2bd2
sharath kumar R <fxpi.sharath@gmail.com>
2019-11-05 15:02:16 +0530

    eslint, prittier, gitignore

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ const Pet = ({name, animal, breed}) => {
 };
 
 
+
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me"),
-            React.createElement(Pet, {name: "Chinnu", animal: "Dog", breed: "Golden Retriever"}),
-            React.createElement(Pet, {name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
-            React.createElement(Pet, {name: "Doink", animal: "Cat", breed: "Mixed"})
-        ]
-    );
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me"),
+        React.createElement(Pet, {
+            name: "Chinnu",
+            animal: "Dog",
+            breed: "Golden Retriever"
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel"
+        }),
+        React.createElement(Pet, {name: "Doink", animal: "Cat", breed: "Mixed"})
+    ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
